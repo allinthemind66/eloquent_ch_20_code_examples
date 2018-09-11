2dcacf822ffbbc555badef8f19a6de693feddbac
allinthemind66 <r.nilon92@gmail.com>
2018-09-11 19:44:34 -0400

    http server

--- main.js
+++ main.js
@@ -6,10 +6,21 @@ const {reverse} = require("./reverse")
 // The second element will be the path to the JavaScript file being executed.
 // The remaining elements will be any additional command line arguments.
 
-
 // The process.execPath property returns the absolute pathname of the executable
 // that started the Node.js process.
 
 let argument = process.argv[2]
 
 console.log(reverse(argument));
+
+
+const {createServer} = require("http");
+let server = createServer((request, response) => {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write(`
+    <h1>Hello!</h1>
+    <p>You asked for <code>${request.url}</code></p>`);
+  response.end();
+});
+server.listen(8000);
+console.log("Listening! (port 8000)");
