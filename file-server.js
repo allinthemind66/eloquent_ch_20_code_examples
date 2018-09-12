2425fdd7fb0c4d0860c2d95bb45b90e6fa65d02d
allinthemind66 <r.nilon92@gmail.com>
2018-09-12 17:11:06 -0400

    add PUT method to methods object

--- file-server.js
+++ file-server.js
@@ -73,3 +73,21 @@ methods.DELETE = async function(request){
   else await unlink(path);
   return {status: 204}
 };
+
+const {createWriteStream} = require('fs');
+
+
+function pipeStream(from, to) {
+  return new Promise((resolve, reject) => {
+    from.on("error", reject);
+    to.on("error", reject);
+    to.on("finish", resolve);
+    from.pipe(to);
+  });
+}
+
+methods.PUT = async function(request) {
+  let path = urlPath(request.url);
+  await pipeStream(request, createWriteStream(path));
+  return {status: 404};
+}
