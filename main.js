b3af4745ab55f1fe61d9ce14663f0b0978f190b4
allinthemind66 <r.nilon92@gmail.com>
2018-09-12 14:35:14 -0400

    add create server

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {reverse} = require("./reverse")
+// const {reverse} = require("./reverse")
 
 //The process.argv property returns an array containing the command line arguments
 // passed when the Node.js process was launched. The first element will be process.execPath.
@@ -9,35 +9,46 @@ const {reverse} = require("./reverse")
 // The process.execPath property returns the absolute pathname of the executable
 // that started the Node.js process.
 
-let argument = process.argv[2]
 
-console.log(reverse(argument));
-
-
-const {createServer} = require("http");
-let server = createServer((request, response) => {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write(`
-    <h1>Hello!</h1>
-    <p>You asked for <code>${request.url}</code></p>`);
-  response.end();
-});
-server.listen(8000);
-console.log("Listening! (port 8000)");
+//process is a global binding
+//argv is argument vector. all arguments passed from the command line
+// let argument = process.argv[2]
+//console.log in node
+//text goes to processes standard output stream
+// console.log(reverse(argument));
 
 
+// const {createServer} = require("http");
+// let server = createServer((request, response) => {
+//   response.writeHead(200, {"Content-Type": "text/html"});
+//   response.write(`
+//     <h1>Hello!</h1>
+//     <p>You asked for <code>${request.url}</code></p>`);
+//   response.end();
+// });
+// server.listen(8000);
+// console.log("Listening! (port 8000)");
+//
+//
+//
+//
+// //can also have a response.write method if its a post method
+// //can also just load node-fetch module for cleaner interface
+// const {request} = require("http");
+// let requestStream = request({
+//   hostname: "eloquentjavascript.net",
+//   path: "/20_node.html",
+//   method: "GET",
+//   headers: {Accept: "text/html"}
+// }, response => {
+//   console.log("The response is ",
+//               response);
+// });
+// requestStream.end();
 
 
-//can also have a response.write method if its a post method
-//can also just load node-fetch module for cleaner interface
-const {request} = require("http");
-let requestStream = request({
-  hostname: "eloquentjavascript.net",
-  path: "/20_node.html",
-  method: "GET",
-  headers: {Accept: "text/html"}
-}, response => {
-  console.log("The response is ",
-              response);
-});
-requestStream.end();
+// const {writeFile} = require("fs");
+// writeFile("anotherfile.txt", "Node was also here", err => {
+//   if (err) console.log(`Failed to write file: ${err}`);
+//   else console.log("File written.");
+// });
