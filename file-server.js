ecbe795e5843cb8f7f3dacc10663f00acda4b70d
allinthemind66 <r.nilon92@gmail.com>
2018-09-12 16:52:07 -0400

    add DELETE method to methods object

--- file-server.js
+++ file-server.js
@@ -57,3 +57,19 @@ methods.GET = async function(request){
       type: mime.getType(path))}
   }
 };
+
+const {rmdir, unlink} = require("fs").promises;
+
+methods.DELETE = async function(request){
+  let path = urlPath(request.url);
+  let stats;
+  try {
+    stats = await stat(path);
+  } catch(error) {
+    if (error.code != "ENOENT") throw error;
+    else return {status: 204}
+  }
+  if (stats.isDirectory()) await rmdir(path);
+  else await unlink(path);
+  return {status: 204}
+};
